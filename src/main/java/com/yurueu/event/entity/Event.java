3b3966a8ec400e50f4cca7c6771295d339b87826
Dobrowydka <andrikchannelyou@gmail.com>
2022-10-31 17:16:53 +0300

    creating Event entity class

---
+++ src/main/java/com/yurueu/event/entity/Event.java
@@ -0,0 +1,38 @@
+package com.yurueu.event.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Data
+@Accessors(chain = true)
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "topic")
+    private String topic;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "organizer")
+    private String organizer;
+
+    @Column(name = "event_date")
+    private LocalDateTime eventDate;
+
+    @Column(name = "place")
+    private String place;
+
+}
