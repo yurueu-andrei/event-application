02493a9901b4847384052887717d4d83d1ee2b28
Dobrowydka <andrikchannelyou@gmail.com>
2022-10-31 15:14:53 +0300

    project initialization

---
+++ src/test/java/com/yurueu/event/TechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurueu.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
