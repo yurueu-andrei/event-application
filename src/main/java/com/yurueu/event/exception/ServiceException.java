19c5c4e0d1466e9d68165890d1b9bbce2677fc6b
Dobrowydka <andrikchannelyou@gmail.com>
2022-11-02 00:13:31 +0300

    format code

--- src/main/java/com/yurueu/event/exception/ServiceException.java
+++ src/main/java/com/yurueu/event/exception/ServiceException.java
@@ -1,6 +1,6 @@
 package com.yurueu.event.exception;
 
-public class ServiceException extends RuntimeException{
+public class ServiceException extends RuntimeException {
     public ServiceException(String message) {
         super(message);
     }
